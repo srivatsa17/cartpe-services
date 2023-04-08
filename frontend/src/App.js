9182e07256d1791a91ddac92b9c2ab641baa03a8
Srivatsa J <srivatsa1199@gmail.com>
2023-04-08 22:03:51 +0530

    Modifying few files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <h1>Hello, Welcome</h1>
+                </Container>
+            </main>
+            <hr/>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
