6e51e6b581ccd6f97dac6af6b72c86a9be8f75aa
Srivatsa J <srivatsa1199@gmail.com>
2023-04-09 21:56:57 +0530

    Adding HomeScreen images and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
     return (
@@ -10,7 +11,7 @@ function App() {
             <Header />
             <main className='py-3'>
                 <Container>
-                    <h1>Hello, Welcome</h1>
+                    <HomeScreen />
                 </Container>
             </main>
             <hr/>
