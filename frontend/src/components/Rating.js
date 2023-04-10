bd5dca54ea0fa8cd8085f523d04ac365fce979c7
Srivatsa J <srivatsa1199@gmail.com>
2023-04-11 00:17:10 +0530

    fixed #4

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,27 +1,45 @@
 import React from "react";
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+import { Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import '../css/Rating.css';
 
 function Rating({ rating, text }) {
     return (
-        <div className="ratings" title={`${rating}/5`}>
-            <span>
-                { rating >= 1 ? <BsStarFill id="star"/> : rating >= 0.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
-            </span>
-            <span>
-                { rating >= 2 ? <BsStarFill id="star"/> : rating >= 1.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
-            </span>
-            <span>
-                { rating >= 3 ? <BsStarFill id="star"/> : rating >= 2.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
-            </span>
-            <span>
-                { rating >= 4 ? <BsStarFill id="star"/> : rating >= 3.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
-            </span>
-            <span>
-                { rating >= 5 ? <BsStarFill id="star"/> : rating >= 4.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
-            </span>
-            <span id="text">{text && text}</span>
-        </div>
+        <Row>
+            <Col>
+                <OverlayTrigger
+                    key="bottom"
+                    placement="bottom"
+                    overlay={
+                        <Tooltip id="tooltip-bottom">
+                            <strong>{rating} out of 5 stars</strong>
+                        </Tooltip>
+                    }
+                >
+                    <div className="ratings">
+                        <span>
+                            { rating >= 1 ? <BsStarFill id="star"/> : rating >= 0.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
+                        </span>
+                        <span>
+                            { rating >= 2 ? <BsStarFill id="star"/> : rating >= 1.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
+                        </span>
+                        <span>
+                            { rating >= 3 ? <BsStarFill id="star"/> : rating >= 2.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
+                        </span>
+                        <span>
+                            { rating >= 4 ? <BsStarFill id="star"/> : rating >= 3.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
+                        </span>
+                        <span>
+                            { rating >= 5 ? <BsStarFill id="star"/> : rating >= 4.5 ? <BsStarHalf id="star"/> : <BsStar id="star"/> }
+                        </span>
+                    </div>
+                </OverlayTrigger>
+            </Col>
+
+            <Col>
+                <span>{text && text}</span>
+            </Col>
+        </Row>
     );
 }
 
