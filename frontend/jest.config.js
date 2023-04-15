59c8f28c778ee3b0710a123526f96c9c892f0215
Srivatsa J <srivatsa1199@gmail.com>
2023-04-15 14:32:14 +0530

    Added test thresholds for jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -10,4 +10,12 @@ module.exports = {
     collectCoverage: true,
     coverageReporters: ["json", "html", "text"],
     collectCoverageFrom: ['<rootDir>/src/components/*.js'],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+      },
 };
