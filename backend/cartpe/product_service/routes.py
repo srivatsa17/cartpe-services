2ac91350cd13549f45f842127c8700b7048fa9c8
Srivatsa J <srivatsa1199@gmail.com>
2023-04-24 23:56:43 +0530

    updated routes.py

--- backend/cartpe/product_service/routes.py
+++ backend/cartpe/product_service/routes.py
@@ -1,37 +1,22 @@
 productsBaseUrl = 'api/v1/products'
 routes = [
     # Products model
-    productsBaseUrl + '',                                       # Get all products
-    productsBaseUrl + '/create',                                # Create a product
-    productsBaseUrl + '/<id>',                                  # Get a particular product
-    productsBaseUrl + '/update/<id>',                           # Update a particular product
-    productsBaseUrl + '/delete/<id>',                           # Delete a particular product
+    productsBaseUrl + '',                                       # Get all products or Create a product
+    productsBaseUrl + '/<id>',                                  # Get/Update/Delete a particular product
 
     # Product categories model
-    productsBaseUrl + '/categories',                            # Get all product categories
-    productsBaseUrl + '/categories/<id>',                       # Get a particular product category
-    productsBaseUrl + '/categories/create',                     # Create a product category
-    productsBaseUrl + '/categories/update/<id>',                # Update a product category
-    productsBaseUrl + '/categories/delete/<id>',                # Delete a product category
+    productsBaseUrl + '/categories',                            # Get all product categories or Create a product category
+    productsBaseUrl + '/categories/<id>',                       # Get/Update/Delete a particular product category
 
     # Product reviews model
-    productsBaseUrl + '/<id>/reviews',                           # Get all reviews for a product 
-    productsBaseUrl + '/<id>/reviews/<review-id>',               # Get a particular review for a product 
-    productsBaseUrl + '/<id>/reviews/create',                    # Create a reviews for a product 
-    productsBaseUrl + '/<id>/reviews/update/<review-id>',        # Update a review for a product 
-    productsBaseUrl + '/<id>/reviews/delete/<review-id>',        # Delete a review for a product 
+    productsBaseUrl + '/reviews?product=<id>',                   # Get all reviews for a product or Create one
+    productsBaseUrl + '/reviews?product=<id>&review=<id>',       # Get/Update/Delete a particular review for a product 
 
     # Product images model
-    productsBaseUrl + '/<id>/images/',                           # Get all images for a product 
-    productsBaseUrl + '/<id>/images/<image-id>',                 # Get a particular images for a product 
-    productsBaseUrl + '/<id>/images/create',                     # Upload an image for a product 
-    productsBaseUrl + '/<id>/images/update/<image-id>',          # Update an image for a product 
-    productsBaseUrl + '/<id>/images/delete/<image-id>',          # Delete an image for a product   
+    productsBaseUrl + '/images?product=<id>',                    # Get all images for a product or Create one
+    productsBaseUrl + '/images/?product=<id>&image=<id>',        # Get/Update/Delete a particular image for a product
 
     # Product discounts model
-    productsBaseUrl + '/<id>/discounts/',                        # Get all discounts for a product 
-    productsBaseUrl + '/<id>/discounts/<discount-id>',           # Get a particular discount for a product 
-    productsBaseUrl + '/<id>/discounts/create',                  # Create a discounts for a product 
-    productsBaseUrl + '/<id>/discounts/update/<discount-id>',    # Update a discount for a product 
-    productsBaseUrl + '/<id>/discounts/delete/<discount-id>',    # Delete a discount for a product 
+    productsBaseUrl + '/discounts?product=<id>',                 # Get all discounts for a product or Create one
+    productsBaseUrl + '/discounts?product=<id>&discount=<id>',   # Get/Update/Delete a particular discount for a product 
 ]
