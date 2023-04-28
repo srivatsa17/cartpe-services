da70e5ec65cd0909ddde5ed38e913a8f66e01295
Srivatsa J <srivatsa1199@gmail.com>
2023-04-28 16:59:20 +0530

    Adhoc removed extra spaces

--- backend/cartpe/product_service/tests/test_product_views.py
+++ backend/cartpe/product_service/tests/test_product_views.py
@@ -13,7 +13,7 @@ client = Client()
 
 class GetAllProductsTest(APITestCase):
     """ Test module for GET all products API """
-    
+
     def setUp(self) -> None:
         Product.objects.create(name = "iphone 13", description = "ok product", brand = "apple", price = 70000, stock_count = 1)
         Product.objects.create(name = "pixel 7", description = "good product", brand = "google", price = 50000, stock_count = 10)
@@ -22,13 +22,13 @@ class GetAllProductsTest(APITestCase):
         response = client.get(reverse('products'))
         products = Product.objects.all()
         serializer = ProductSerializer(products, many = True)
-        
+
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 class GetProductByIdTest(APITestCase):
     """ Test module for GET product by id API """
-    
+
     def setUp(self) -> None:
         self.iphone = Product.objects.create(name = "iphone 13", description = "ok product", brand = "apple", price = 70000, stock_count = 1)
         self.pixel = Product.objects.create(name = "pixel 7", description = "good product", brand = "google", price = 50000, stock_count = 10)
@@ -37,19 +37,19 @@ class GetProductByIdTest(APITestCase):
         response = client.get(reverse('product_by_id', kwargs = { 'id' : self.pixel.id }))
         product = Product.objects.get(id = self.pixel.pk)
         serializer = ProductSerializer(product, many = False)
-        
+
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_product_by_id_with_non_existing_id(self) -> None:
         response = client.get(reverse('product_by_id', kwargs = { 'id' : 30 }))
-        
+
         self.assertEqual(response.data['message'], "Unable to find product with id 30")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    
+
 class PostProductTest(APITestCase):
     """ Test module for Post a product API """
-    
+
     def setUp(self) -> None:
         self.category = Category.objects.create(name = "Electronics")
         self.validData = { "name":"DSLR", "description":"Amazing", "brand":"Cannon", "category":"Electronics", "price":929.99, "stock_count":5, "discount":0 }
@@ -78,7 +78,7 @@ class PostProductTest(APITestCase):
 
         self.assertEqual(response.data['description'][0], "This field is required.")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
     def test_post_product_with_missing_price(self) -> None:
         response = client.post(reverse('products'), data = json.dumps(self.dataWithNoPriceField), content_type = 'application/json')
 
@@ -108,7 +108,7 @@ class PostProductTest(APITestCase):
 
         self.assertEqual(response.data['category'][0], "This field is required.")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
     def test_post_product_with_same_name(self) -> None:
         client.post(reverse('products'), data = json.dumps(self.validData), content_type = 'application/json')
         response = client.post(reverse('products'), data = json.dumps(self.dataWithExistingName), content_type = 'application/json')
@@ -137,55 +137,55 @@ class UpdateProductByIdTest(APITestCase):
 
     def test_update_product_id(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateId), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['id'], self.pixel.id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_product_name(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateName), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['name'], "abc")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
+
     def test_update_product_sku(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateSku), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['sku'], str(self.pixel.sku))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
+
     def test_update_product_description(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateDescription), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['description'], self.updateDescription['description'])
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_product_slug(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateSlug), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['slug'], self.pixel.slug)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_product_price(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updatePrice), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['price'], decimal.Decimal('70000.90'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_product_brand(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateBrand), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['brand'], "apple")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_product_stock_count(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateStockCount), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['stock_count'], 15)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_product_discount(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateDiscount), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['discount'], 10)
         self.assertEqual(response.data['discounted_price'], decimal.Decimal('5000'))
         self.assertEqual(response.data['selling_price'], 45000)
@@ -194,25 +194,25 @@ class UpdateProductByIdTest(APITestCase):
     def test_update_product_category(self) -> None:
         self.category = Category.objects.create(name = "Phone")
         response = client.patch(reverse('product_by_id', kwargs={'id' : self.pixel.id}), data = json.dumps(self.updateCategory), content_type = 'application/json')
-        
+
         self.assertEqual(str(response.data['category']), "Phone")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_product_name_with_empty_input(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id': self.pixel.id}), data = json.dumps(self.invalidName), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['name'][0], "This field may not be blank.")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_update_product_with_nonexisting_id(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs = {'id': 30}), data = json.dumps(self.invalidName), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['message'], "Unable to find product with id 30")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_update_product_with_same_name(self) -> None:
         response = client.patch(reverse('product_by_id', kwargs={'id': self.pixel.id}), data = json.dumps(self.existingData), content_type = 'application/json')
-        
+
         self.assertEqual(response.data['message'][0], "Product 'pixel 7' already exists and cannot be created or updated again.")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -224,12 +224,12 @@ class DeleteProductByIdTest(APITestCase):
 
     def test_delete_with_existing_id(self) -> None:
         response = client.delete(reverse('product_by_id', kwargs={'id': self.pixel.id}))
-        
+
         self.assertEqual(response.data['message'], "Product 'pixel 7' deleted successfully.")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_delete_with_nonexisting_id(self) -> None:
         response = client.delete(reverse('product_by_id', kwargs={'id': 30}))
-        
+
         self.assertEqual(response.data['message'], "Unable to find product with id 30")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- backend/cartpe/product_service/views.py
+++ backend/cartpe/product_service/views.py
@@ -7,26 +7,26 @@ from .models import Product
 
 # Create your views here.
 
-class RoutesAPIView(generics.GenericAPIView): 
+class RoutesAPIView(generics.GenericAPIView):
     def get(self, request):
         return Response(routes)
 
 class ProductAPIView(generics.GenericAPIView):
 
     serializer_class = ProductSerializer
-    
+
     def get(self, request):
         products = Product.objects.all()
         serializer = self.serializer_class(products, many = True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         serializer = self.serializer_class(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-    
+
 class ProductByIdAPIView(generics.GenericAPIView):
 
     serializer_class = ProductSerializer
@@ -37,12 +37,12 @@ class ProductByIdAPIView(generics.GenericAPIView):
         except Product.DoesNotExist:
             response = { "message" : "Unable to find product with id " + str(id) }
             raise NotFound(response)
-        
+
     def get(self, request, id):
         product = self.get_object(id)
         serializer = self.serializer_class(product, many = False)
         return Response(serializer.data)
-    
+
     def patch(self, request, id):
         product = self.get_object(id)
         serializer = self.serializer_class(product, data = request.data, partial = True)
@@ -50,7 +50,7 @@ class ProductByIdAPIView(generics.GenericAPIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-    
+
     def delete(self, request, id):
         product = self.get_object(id)
         product.delete()
