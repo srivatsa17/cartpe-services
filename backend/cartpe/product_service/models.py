016c33b6c4960e1465f4fcbd2cebfb5278f51390
Srivatsa J <srivatsa1199@gmail.com>
2023-04-30 19:29:46 +0530

    Added Brand model

--- backend/cartpe/product_service/models.py
+++ backend/cartpe/product_service/models.py
@@ -22,13 +22,27 @@ class Category(MPTTModel):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
+class Brand(models.Model):
+    name = models.CharField(max_length = 255, unique = True, null = False, blank = False)
+    slug = models.SlugField(max_length = 255, null = True, blank = True)
+    description = models.TextField(null = False, blank = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Brand, self).save(*args, **kwargs)
+
 class Product(models.Model):
     sku = models.UUIDField(primary_key = False, default = uuid.uuid4, editable = False)
     name = models.CharField(max_length = 255, unique = True, null = False, blank = False)
     slug = models.SlugField(max_length = 255, null = True, blank = True)
     description = models.TextField(null = False, blank = False)
     price = models.DecimalField(max_digits = 7, decimal_places = 2, null = False, blank = False)
-    brand = models.CharField(max_length = 255, null = False, blank = False)
+    brand = models.ForeignKey(Brand, on_delete = models.SET_NULL, null = True, blank = True, related_name = 'products')
     category = models.ForeignKey(Category, on_delete = models.SET_NULL, null = True, blank = True, related_name = 'products')
     discount = models.PositiveSmallIntegerField(default = 0, null = False, blank = False)
     stock_count = models.PositiveIntegerField(default = 0, null = False, blank = False)
