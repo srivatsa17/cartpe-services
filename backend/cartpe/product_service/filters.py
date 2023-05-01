09d7b568f58184c4a2ea0544fc446483f66f5673
Srivatsa J <srivatsa1199@gmail.com>
2023-05-01 17:13:15 +0530

    Added discount field for filtering product model

--- backend/cartpe/product_service/filters.py
+++ backend/cartpe/product_service/filters.py
@@ -1,9 +1,10 @@
-from django_filters import FilterSet, CharFilter
+from django_filters import FilterSet, CharFilter, NumberFilter
 from .models import Product
 
 class ProductFilter(FilterSet):
     brand = CharFilter(field_name='brand__name', lookup_expr='iexact')
     category = CharFilter(field_name='category__name', lookup_expr='iexact')
+    discount = NumberFilter(field_name='discount', lookup_expr='gte')
 
     class Meta:
         model = Product
