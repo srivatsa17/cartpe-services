d2953f0d035179bc83c63f911d81217632514809
Srivatsa J <srivatsa1199@gmail.com>
2023-05-17 20:21:04 +0530

    added attribute values for filtering

--- backend/cartpe/product_service/filters.py
+++ backend/cartpe/product_service/filters.py
@@ -1,10 +1,11 @@
 from django_filters import FilterSet, CharFilter, NumberFilter
-from .models import Product, Category
+from .models import Product, Category, AttributeValue
 
 class ProductFilter(FilterSet):
     brand = CharFilter(field_name='brand__name', lookup_expr='iexact')
     category = CharFilter(method='filter_category_name', label='Category name')
     discount = NumberFilter(field_name='discount', lookup_expr='gte')
+    attribute = CharFilter(method='filter_attribute_value', label='Attribute name')
 
     class Meta:
         model = Product
@@ -12,8 +13,13 @@ class ProductFilter(FilterSet):
 
     def filter_category_name(self, queryset, name, value):
         return queryset.filter(
-                    category__in=Category.objects.filter(name__iexact=value).get_descendants(include_self=True)
-                )
+            category__in = Category.objects.filter(name__iexact = value).get_descendants(include_self = True)
+        )
+
+    def filter_attribute_value(self, queryset, name, value):
+        return queryset.filter(
+            attributes__attribute_values__in = AttributeValue.objects.filter(value__iexact = value)
+        )
 
 class CategoryFilter(FilterSet):
     class Meta:
