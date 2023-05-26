c3ca393d5565049fc18c159146388ad12a982864
Srivatsa J <srivatsa1199@gmail.com>
2023-05-26 15:41:31 +0530

    Added tooltips to filters

--- frontend/src/components/ProductScreen/Filters/CollapsibleList.js
+++ frontend/src/components/ProductScreen/Filters/CollapsibleList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Row } from 'react-bootstrap';
+import { Row, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import '../../../css/ProductSearchScreen/Filters/CollapsibleList.css';
 
 function CollapsibleList({ title, children, actionButton, defaultVisible}) {
@@ -9,7 +9,13 @@ function CollapsibleList({ title, children, actionButton, defaultVisible}) {
         <Row>
             <div className="collapsible-container">
                 <button className="collapsible-button" onClick={() => setVisible((visible) => !visible)}>
-                    <div>{title}</div>
+                    {   !visible ? 
+                            <OverlayTrigger placement="top" overlay={ <Tooltip>Click to apply filters</Tooltip> }>
+                                <div>{title}</div>
+                            </OverlayTrigger> 
+                        :
+                            <div>{title}</div>
+                    }
                 </button>
                 <div>{actionButton}</div>
             </div>
