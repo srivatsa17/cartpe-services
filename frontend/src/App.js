5d3edcd98db5c52fb87200e84de8ad59ea0a3ac3
Srivatsa J <srivatsa1199@gmail.com>
2023-06-25 16:42:30 +0530

    Added register user page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,29 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Header/Navbar';
-import Footer from './components/Footer/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductSearchScreen from './screens/ProductSearchScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import UserRegisterScreen from './screens/UserRegisterScreen';
+import RouteWithNavbar from './RouteWithNavbar';
+import RouteWithoutNavbar from './RouteWithoutNavbar';
 
 function App() {
 
     return (
         <Router>
-            <Navbar />
-            <main className='py-3'>
-                <Container>
-                    <Routes>
-                        <Route path='/' element={<HomeScreen />} exact />
-                        <Route path='/:slug' element={<ProductSearchScreen />}/>
-                        <Route path='/products/:slug/:id/buy' element={<ProductScreen />} />
-                        <Route path='/cart' element={<CartScreen />} />
-                    </Routes>
-                </Container>
-            </main>
-            <hr/>
-            <Footer />
+            <Routes>
+                <Route element={<RouteWithoutNavbar />}>
+                    <Route path='/user/register' element={<UserRegisterScreen />}/>
+                </Route>
+                <Route element={<RouteWithNavbar />} >
+                    <Route path='/' element={<HomeScreen />} exact />
+                    <Route path='/:slug' element={<ProductSearchScreen />}/>
+                    <Route path='/products/:slug/:id/buy' element={<ProductScreen />} />
+                    <Route path='/cart' element={<CartScreen />} />
+                </Route>
+            </Routes>
         </Router>
     );
 }
