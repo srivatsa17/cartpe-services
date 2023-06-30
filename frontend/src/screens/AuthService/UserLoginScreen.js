a778350bff41fd48aa15cce2e938ed8bd67bb0d8
Srivatsa J <srivatsa1199@gmail.com>
2023-07-01 01:12:27 +0530

    Updating login screen code

--- frontend/src/screens/AuthService/UserLoginScreen.js
+++ frontend/src/screens/AuthService/UserLoginScreen.js
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom';
-import { Button, Row, Col, Form, InputGroup, Image, OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { useDispatch } from "react-redux";
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { loginUser } from '../../actions/authActions';
 import "../../css/AuthService/Login/UserLoginScreen.css";
+
+import { Button, Col, Form, Image, InputGroup, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { HOME_SCREEN, REGISTER_USER_SCREEN, RESET_PASSWORD_SCREEN } from "../../constants/routes";
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import ErrorMessage from "../../components/ErrorMessages/ErrorMessage";
+import Loader from "../../components/Loader/Loader";
+import { loginUser } from '../../actions/authActions';
 
 function UserLoginScreen() {
     const loginUserImage = "/images/login.png";
+    const navigate = useNavigate();
     const dispatch = useDispatch();
+
     const [formData, setFormData] = useState({
         email: '',
         password: '',
@@ -53,17 +60,32 @@ function UserLoginScreen() {
         return "";
     }
 
+    const userDetails = useSelector(state => state.userDetails)
+    const { error, loading, isLoggedIn } = userDetails
+
+    useEffect(() => {
+        if(isLoggedIn === true) {
+            navigate(HOME_SCREEN);
+        }
+    }, [isLoggedIn, navigate])
+
+    const handleLoginClick = (event) => {
+        event.preventDefault();
+        dispatch(loginUser(email, password));
+    }
+
     return (
         <Row className="login-user-container">
             <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                 <Image className="login-user-image" src={loginUserImage} alt="login" />
             </Col>
             <Col lg={5} xl={4}>
+                { error && <ErrorMessage>{error}</ErrorMessage> }
                 <div className="login-heading">
                     Login to <span id="brand-name">CartPe</span>!
                 </div>
                 <Form.Group>
-                    <Form.Label className="email-label">Email</Form.Label>
+                    <Form.Label htmlFor="email" className="email-label">Email</Form.Label>
                     <Form.Control
                         type="email"
                         className={email.length > 0 && (isEmailValid ? 'is-valid' : 'is-invalid')}
@@ -72,14 +94,15 @@ function UserLoginScreen() {
                         onChange={handleEmailChange}
                         required
                     />
-                    {   isEmailValid ?
+                    {   isEmailValid ? (
                             <Form.Control.Feedback className="valid-feedback py-1">
                                 Email looks good!
                             </Form.Control.Feedback>
-                        :
+                        ) : (
                             <Form.Control.Feedback className="invalid-feedback py-1">
                                 {invalidEmailMessage}
                             </Form.Control.Feedback>
+                        )
                     }
                 </Form.Group>
                 <Form.Group>
@@ -110,34 +133,53 @@ function UserLoginScreen() {
                             </OverlayTrigger>
                         </InputGroup.Text>
                         {
-                            isPasswordValid ?
+                            isPasswordValid ? (
                                 <Form.Control.Feedback className="valid-feedback py-1">
                                     Password looks good!
                                 </Form.Control.Feedback>
-                            :
+                            ) : (
                                 <Form.Control.Feedback className="invalid-feedback py-1">
                                     {invalidPasswordMessage(password)}
                                 </Form.Control.Feedback>
+                            )
                         }
                     </InputGroup>
                 </Form.Group>
-                <Button
-                    variant="dark"
-                    className="login-button"
-                    disabled={!(isEmailValid && isPasswordValid)}
-                    onClick={() => dispatch(loginUser(email, password))}
-                >
-                    Login
-                </Button>
+                {
+                    loading ?
+                    <Button
+                        variant="dark"
+                        className="login-button"
+                        disabled
+                    >
+                        <Loader
+                            as="span"
+                            animation="border"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"
+                        />
+                        <span className="mx-2">Loading...</span>
+                    </Button>
+                    :
+                    <Button
+                        variant="dark"
+                        className="login-button"
+                        disabled={!(isEmailValid && isPasswordValid)}
+                        onClick={handleLoginClick}
+                    >
+                        Login
+                    </Button>
+                }
                 <div className="reset-password-link-container">
                     Can't remember password?
-                    <Link to="/user/reset-password" className="reset-password-link-button">
+                    <Link to={RESET_PASSWORD_SCREEN} className="reset-password-link-button">
                         Reset password
                     </Link>
                 </div>
                 <div className="register-link-container">
                     Don't have an account?
-                    <Link to="/user/register" className="register-link-button">
+                    <Link to={REGISTER_USER_SCREEN} className="register-link-button">
                         Register
                     </Link>
                 </div>
