50e0ff6b5d23e2460e71c8599d07f906ed97c079
Srivatsa J <srivatsa1199@gmail.com>
2024-05-20 19:22:28 +0530

    Add product review model

--- backend/cartpe/product_service/models.py
+++ backend/cartpe/product_service/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 import uuid
 from django.utils.text import slugify
+from django.core.validators import MinValueValidator, MaxValueValidator
 from mptt.models import MPTTModel, TreeForeignKey
 from auth_service.models import User
 
@@ -101,6 +102,18 @@ class ProductVariant(models.Model):
 class WishList(models.Model):
     product_variant = models.ForeignKey(ProductVariant, on_delete = models.CASCADE, null = False, blank = False, related_name = 'wishlist')
     user = models.ForeignKey(User, on_delete = models.CASCADE, null = False, blank = False, related_name = 'wishlist')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return str(self.pk)
+
+class ProductReview(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE, null = False, blank = False, related_name = 'product_review')
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = False, blank = False, related_name = 'product_review')
+    headline = models.CharField(max_length = 255, null = False, blank = False)
+    rating = models.PositiveSmallIntegerField(null = False, blank = False, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField(null = True, blank = True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
