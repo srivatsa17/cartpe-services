37a78c544d53d62f58cc122e94c1600d592d7b7c
Srivatsa J <srivatsa1199@gmail.com>
2024-06-07 16:04:14 +0530

    Add tests for reset password

---
+++ backend/cartpe/tests/AuthServiceTests/ResetPasswordConfirmTests/test_post_api.py
@@ -0,0 +1,122 @@
+from django.urls import reverse
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+import json
+from auth_service.models import User
+
+# Global variables
+CONTENT_TYPE = "application/json"
+
+# Initialize the APIClient app
+client = APIClient()
+
+class ResetPasswordConfirmTest(APITestCase):
+    """ Test module for POST request for ResetPasswordConfirmAPIView API """
+
+    def get_url(self):
+        url = reverse("reset-password-confirm")
+        return url
+
+    def setUp(self):
+        self.user = User.objects.create(email = "testuser@example.com", password = "abcdefgh")
+
+    def test_success(self):
+        url = self.get_url()
+        data = json.dumps({
+            "new_password": "testuser123",
+            "confirm_new_password": "testuser123",
+            "uid" : urlsafe_base64_encode(force_bytes(self.user.pk)),
+            "token" : default_token_generator.make_token(self.user)
+        })
+
+        response = client.post(url, data = data, content_type = CONTENT_TYPE)
+
+        self.assertEqual("Password reset successful.", response.data["message"])
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_with_non_alphanumeric_passwords(self):
+        url = self.get_url()
+        data = json.dumps({
+            "new_password": "testuser",
+            "confirm_new_password": "testuser",
+            "uid" : urlsafe_base64_encode(force_bytes(self.user.pk)),
+            "token" : default_token_generator.make_token(self.user)
+        })
+
+        response = client.post(url, data = data, content_type = CONTENT_TYPE)
+
+        self.assertEqual("Password should contain alphabets and digits.", response.data["message"][0])
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_with_unmatched_passwords(self):
+        url = self.get_url()
+        data = json.dumps({
+            "new_password": "testuser1234",
+            "confirm_new_password": "testuser123",
+            "uid" : urlsafe_base64_encode(force_bytes(self.user.pk)),
+            "token" : default_token_generator.make_token(self.user)
+        })
+
+        response = client.post(url, data = data, content_type = CONTENT_TYPE)
+
+        self.assertEqual("New passwords not matching.", response.data["message"][0])
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_with_invalid_uid(self):
+        url = self.get_url()
+        data = json.dumps({
+            "new_password": "testuser123",
+            "confirm_new_password": "testuser123",
+            "uid" : "abcd123",
+            "token" : default_token_generator.make_token(self.user)
+        })
+
+        response = client.post(url, data = data, content_type = CONTENT_TYPE)
+
+        self.assertEqual("Error occurred while decoding base64 user id", response.data["message"][0])
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_with_invalid_decoded_pk(self):
+        url = self.get_url()
+        data = json.dumps({
+            "new_password": "testuser123",
+            "confirm_new_password": "testuser123",
+            "uid" : urlsafe_base64_encode(force_bytes(self.user.pk)) + "a",
+            "token" : default_token_generator.make_token(self.user)
+        })
+
+        response = client.post(url, data = data, content_type = CONTENT_TYPE)
+
+        self.assertEqual("Invalid type received for user id",response.data["message"][0] )
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_with_non_existing_user(self):
+        url = self.get_url()
+        data = json.dumps({
+            "new_password": "testuser123",
+            "confirm_new_password": "testuser123",
+            "uid" : urlsafe_base64_encode(force_bytes(123)),
+            "token" : default_token_generator.make_token(self.user)
+        })
+
+        response = client.post(url, data = data, content_type = CONTENT_TYPE)
+
+        self.assertEqual("Unable to find user", response.data["message"][0])
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
+
+    def test_with_invalid_token(self):
+        url = self.get_url()
+        data = json.dumps({
+            "new_password": "testuser123",
+            "confirm_new_password": "testuser123",
+            "uid" : urlsafe_base64_encode(force_bytes(self.user.pk)),
+            "token" : "invalid_token"
+        })
+
+        response = client.post(url, data = data, content_type = CONTENT_TYPE)
+
+        self.assertEqual("Invalid or expired token", response.data["message"][0])
+        self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
---
+++ backend/cartpe/tests/AuthServiceTests/ResetPasswordRequestTests/test_email.py
@@ -0,0 +1,75 @@
+from django.test import TestCase
+from unittest.mock import patch, MagicMock
+from auth_service.models import User
+from auth_service.email import send_reset_password_email
+from cartpe import settings
+
+class SendResetPasswordEmailTest(TestCase):
+    """ Test module for send_reset_password_email method """
+
+    def setUp(self):
+        self.user_email = "testuser@example.com"
+        self.current_site = "127.0.0.1:3000"
+
+    def test_user_does_not_exist(self):
+        response = send_reset_password_email(self.user_email)
+
+        self.assertEqual(400, response["status"])
+
+    @patch("auth_service.email.render_to_string")
+    @patch("auth_service.email.EmailMultiAlternatives")
+    def test_email_sent_successfully(self, mock_email_class, mock_render_to_string):
+        User.objects.create_user(
+            email = "testuser@example.com", password = "test@123",
+            first_name = "test_user", last_name = "test_user"
+        )
+
+        # Mock render_to_string
+        mock_render_to_string.return_value = "<html>Reset your password</html>"
+
+        # Mock EmailMultiAlternatives
+        mock_email_instance = MagicMock()
+        mock_email_class.return_value = mock_email_instance
+
+        response = send_reset_password_email(self.user_email)
+
+        self.assertEqual(response["status"], 200)
+        mock_render_to_string.assert_called_once()
+        mock_email_class.assert_called_once_with(
+            subject="Reset your password",
+            body="Reset your password",
+            from_email="CartPe <%s>" % settings.EMAIL_HOST_USER,
+            to=[self.user_email]
+        )
+        mock_email_instance.attach_alternative.assert_called_once_with("<html>Reset your password</html>", "text/html")
+        mock_email_instance.send.assert_called_once()
+
+    @patch("auth_service.email.render_to_string")
+    @patch("auth_service.email.EmailMultiAlternatives")
+    def test_exception_during_email_sending(self, mock_email_class, mock_render_to_string):
+        User.objects.create_user(
+            email = "testuser@example.com", password = "test@123",
+            first_name = "test_user", last_name = "test_user"
+        )
+
+        # Mock render_to_string
+        mock_render_to_string.return_value = "<html>Reset your password</html>"
+
+        # Mock EmailMultiAlternatives to raise an exception
+        mock_email_instance = MagicMock()
+        mock_email_instance.send.side_effect = Exception("Email sending failed")
+        mock_email_class.return_value = mock_email_instance
+
+        response = send_reset_password_email(self.user_email)
+
+        self.assertEqual(response["status"], 400)
+        self.assertEqual(response["error"], "Email sending failed")
+        mock_render_to_string.assert_called_once()
+        mock_email_class.assert_called_once_with(
+            subject="Reset your password",
+            body="Reset your password",
+            from_email="CartPe <%s>" % settings.EMAIL_HOST_USER,
+            to=[self.user_email]
+        )
+        mock_email_instance.attach_alternative.assert_called_once_with("<html>Reset your password</html>", "text/html")
+        mock_email_instance.send.assert_called_once()
