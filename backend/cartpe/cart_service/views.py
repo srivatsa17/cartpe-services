ccdbc672d4fe3d389a5ac84178192800ac4384fc
Srivatsa J <srivatsa1199@gmail.com>
2024-07-10 23:01:45 +0530

    Modify cart service to use django's builtin cache methods

--- backend/cartpe/cart_service/views.py
+++ backend/cartpe/cart_service/views.py
@@ -3,10 +3,7 @@ from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from cart_service.routes import routes
 from cart_service.serializers import CartSerializer, CartByIdSerializer
-import redis
-
-# Initialise JSON redis instance
-redis_client = redis.Redis().json()
+from django.core.cache import cache
 
 class RoutesAPIView(generics.GenericAPIView):
     queryset = routes
@@ -18,105 +15,89 @@ class CartAPIView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = CartSerializer
 
-    def get_object(self):
-        return self.request.user.id
+    def get_redis_cache_key(self):
+        return "user:{user}:cart".format(user = self.request.user)
 
-    def get_redis_key(self):
-        user_id = self.get_object()
-        return "cart:%s" % user_id
-
-    def get_serializer_context(self):
-        return {
-            "redis_key": self.get_redis_key()
-        }
-
-    def get_cart_list(self, redis_key):
-        cart_items = redis_client.get(redis_key)
-        if cart_items is None:
-            # Setting an empty cart if redis response is null.
-            return { "cartItems": [] }
-        return cart_items
+    def get_default_response(self):
+        default_response = { "cartItems": [] }
+        return default_response
 
     """
     Get the list of cartItems for a user
     """
     def get(self, request):
-        redis_key = self.get_redis_key()
-        response = self.get_cart_list(redis_key)
+        response = cache.get(key=self.get_redis_cache_key(), default=self.get_default_response())
         return Response(response, status = status.HTTP_200_OK)
 
     """
     Add the new cartItem at the tail of the list of cartItems
     """
     def post(self, request):
-        redis_key = self.get_redis_key()
-        context = self.get_serializer_context()
+        cart = cache.get(self.get_redis_cache_key(), default=self.get_default_response())
+
+        context = { "cart": cart }
         serializer = self.serializer_class(data = request.data, context = context)
 
         if serializer.is_valid():
-            redis_client.arrappend(redis_key, '$.cartItems', serializer.validated_data)
-            response = self.get_cart_list(redis_key)
-            return Response(response, status=status.HTTP_201_CREATED)
+            cart["cartItems"].append(serializer.validated_data)
+
+            cache.set(key=self.get_redis_cache_key(), value=cart, timeout=None)
+
+            return Response(cart, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     """
     Empty the cart by deleting the redis_key
     """
     def delete(self, request):
-        redis_key = self.get_redis_key()
-        redis_client.delete(redis_key)
+        cache.delete(self.get_redis_cache_key())
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CartByIdAPIView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = CartByIdSerializer
 
-    def get_object(self):
-        return self.request.user.id
+    def get_redis_cache_key(self):
+        return "user:{user}:cart".format(user = self.request.user)
 
-    def get_redis_key(self):
-        user_id = self.get_object()
-        return "cart:%s" % user_id
-
-    def get_serializer_context(self, redis_key, product_id, request_method):
-        return {
-            "request_method": request_method,
-            "redis_key": redis_key,
-            "product_id": product_id
-        }
-
-    def get_cart_list(self, redis_key):
-        cart_items = redis_client.get(redis_key)
-        if cart_items is None:
-            # Setting an empty cart if redis response is null.
-            return { "cartItems": [] }
-        return cart_items
+    def get_default_response(self):
+        default_response = { "cartItems": [] }
+        return default_response
 
     """
     Update the quantity of a particular cartItem in the list of cartItems
     """
     def patch(self, request, id):
-        redis_key = self.get_redis_key()
-        context = self.get_serializer_context(redis_key, id, request.method)
+        cart = cache.get(self.get_redis_cache_key(), default=self.get_default_response())
+
+        context = { "cart": cart, "product_id": id }
         serializer = self.serializer_class(data = request.data, context = context)
 
         if serializer.is_valid():
-            index = serializer.validated_data['index']
-            quantity = serializer.validated_data['quantity']
-            redis_client.execute_command('JSON.SET', redis_key, '$.cartItems[%d].quantity' % index, quantity)
-            return Response(self.get_cart_list(redis_key), status=status.HTTP_200_OK)
+            [
+                cart_item.update({ "quantity": serializer.validated_data["quantity"] }) \
+                for cart_item in cart["cartItems"] if cart_item["product"]["id"] == id
+            ]
+
+            cache.set(key=self.get_redis_cache_key(), value=cart, timeout=None)
+
+            return Response(cart, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     """
     Remove the cartItem from the list of cartItems
     """
     def delete(self, request, id):
-        redis_key = self.get_redis_key()
-        context = self.get_serializer_context(redis_key, id, request.method)
+        cart = cache.get(self.get_redis_cache_key(), default=self.get_default_response())
+
+        context = { "cart": cart, "product_id": id }
         serializer = self.serializer_class(data = request.data, context = context)
 
         if serializer.is_valid():
-            index = serializer.validated_data['index']
-            redis_client.delete(redis_key, '$.cartItems[%d]' % index)
-            return Response(self.get_cart_list(redis_key), status=status.HTTP_200_OK)
+            new_cart_items = [cart_item for cart_item in cart["cartItems"] if cart_item["product"]["id"] != id]
+            cart["cartItems"] = new_cart_items
+
+            cache.set(key=self.get_redis_cache_key(), value=cart, timeout=None)
+
+            return Response(cart, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
